92adbb09e8c0f3ef8e50cd565796d4b476dac9b6
Sri Srivatsavaya <srivatsavaya.varma@outlook.com>
2021-02-08 14:53:23 -0500

    Committing to master
    Committing to master

---
+++ src/main/java/org/syspar/tests/Test.java
@@ -0,0 +1,12 @@
+package org.syspar.tests;
+
+public class Test {
+
+    public Test() {
+        // Empty Constructor
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Test World!!!");
+    }
+}
